5e28ff03c3192d8aec88cbe199ea327d69ac2958
Gia Lac <duonggialac66@gmail.com>
2026-06-05 16:06:05 +0700

    feat: initialize Prisma ORM and generate client files

--- api/db.ts
+++ api/db.ts
@@ -1,3 +1,11 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/prisma';
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
 export default prisma;
